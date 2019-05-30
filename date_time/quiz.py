f7af4c7f67f05af7d75c0e4ac30d1ae7b6fcdefe
Peter Pallen <peter@ppallen.be>
2019-05-30 22:59:47 +0200

    Added typings to the file
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- date_time/quiz.py
+++ date_time/quiz.py
@@ -1,4 +1,5 @@
 from questions import Add, Multiply
+from typing import Tuple, Union
 import datetime
 import random
 
@@ -8,15 +9,15 @@ class Quiz:
     answers: list = []
 
     def __init__(self):
-        question_types = (Add, Multiply)
+        question_types: tuple = (Add, Multiply)
         # generate 10 random questions with numbers from 1 to 10
         for _ in range(10):
-            num1 = random.randint(1, 10)
-            num2 = random.randint(1, 10)
-            question = random.choice(question_types)(num1, num2)
+            num1: int = random.randint(1, 10)
+            num2: int = random.randint(1, 10)
+            question: tuple = random.choice(question_types)(num1, num2)
             self.questions.append(question)
 
-    def take_quiz(self):
+    def take_quiz(self) -> str:
         self.start_time = datetime.datetime.now()
 
         for question in self.questions:
@@ -25,7 +26,9 @@ class Quiz:
             self.end_time = datetime.datetime.now()
         return self.summary()
 
-    def ask(self, question):
+    def ask(self,
+            question: Union[Add, Multiply]
+            ) -> Tuple[bool, datetime.timedelta]:
         correct = False
         question_start = datetime.datetime.now()
         answer = input(question.text + ' = ')
@@ -36,9 +39,9 @@ class Quiz:
         question_end = datetime.datetime.now()
         return correct, question_end - question_start
 
-    def total_correct(self):
+    def total_correct(self) -> int:
         # return the total # of correct answers
-        total = 0
+        total: int = 0
         for answer in self.answers:
             if answer[0]:
                 total += 1
