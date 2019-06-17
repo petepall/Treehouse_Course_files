9560ef3e1dd4a043ff751e944115dc5962857984
Peter Pallen <peter@ppallen.be>
2019-06-17 20:40:21 +0200

    Optimized the reading and display of header
    Signed-off-by: Peter Pallen <peter@ppallen.be>

--- file_processing/read_csv.py
+++ file_processing/read_csv.py
@@ -2,18 +2,20 @@ import csv
 from pathlib import Path
 
 
-with open(f"{Path().absolute()}//teachers.csv", mode='r') as csv_file:
+with open(f"{Path().absolute()}\\file_processing\\teachers.csv",
+          mode='r') as csv_file:
+
     # construct the header line
-    header = csv_file.readline().split(',')
-    print(f"{header[0].strip():15}{header[1].strip():15}"
-          f"{header[2].strip():15}", end='\n')
-    print(f"{'*' * len(header[0].strip()):15}"
-          f"{'*' * len(header[1].strip()):15}"
-          f"{'*' * len(header[2].strip()):15}", end='\n')
+    data_reader = csv.DictReader(csv_file, delimiter=',')
+    print(f"{data_reader.fieldnames[0].capitalize():15}"
+          f"{data_reader.fieldnames[1].capitalize():15}"
+          f"{data_reader.fieldnames[2].capitalize():15}")
+
+    print(f"{'*' * len(data_reader.fieldnames[0].strip()):15}"
+          f"{'*' * len(data_reader.fieldnames[1].strip()):15}"
+          f"{'*' * len(data_reader.fieldnames[2].strip()):15}", end='\n')
 
     # display the detail lines
-    csv_file.seek(0)
-    data_reader = csv.DictReader(csv_file, delimiter=',')
     rows = list(data_reader)
     for row in rows[:]:
         print(f"{row['first_name'].strip():15}{row['last_name'].strip():15}"
